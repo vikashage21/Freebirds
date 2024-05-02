23b09628802fe30e3463627cec0ebd91cc3488e2
vikash sharma <vk220783@gmail.com>
2024-05-02 10:04:05 +0530

    code for error handling

---
+++ src/middlewares/error_middleware.js
@@ -0,0 +1,14 @@
+const errormiddle =(err,req, res, next)=>{
+    const status= err.status || 500;
+
+    const message = err.message || "backend error "
+
+    const extraDetails = err.extraDetails || "Error from Backend";
+
+    return res.status (status).json({
+        message, extraDetails
+    })
+
+}
+
+export {errormiddle}
